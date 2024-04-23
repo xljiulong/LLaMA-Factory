db5f8fb07e8c6d5192e6e0aba2fd5029802dde77
zhangjl19 <zhangjl19@spdb.com.cn>
2024-04-23 13:49:40 +0800

    add codes

--- scripts/run_sft_lora.sh
+++ scripts/run_sft_lora.sh
@@ -2,7 +2,7 @@ WANDB_DISABLED=1 NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 deepspeed --num_gpus 2 --m
     --stage sft \
     --do_train \
     --deepspeed /workspace/projects/LLaMA-Factory/examples/deepspeed/ds_z3_config.json \
-    --model_name_or_path /workspace/models/huggingface/chatglm3-6b-32k \
+    --model_name_or_path /workspace/models/huggingface/chatglm3-6b \
     --dataset alpaca_gpt4_zh \
     --dataset_dir /workspace/projects/LLaMA-Factory/data \
     --template chatglm3 \
@@ -13,8 +13,8 @@ WANDB_DISABLED=1 NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 deepspeed --num_gpus 2 --m
     --overwrite_output_dir \
     --cutoff_len 1024 \
     --preprocessing_num_workers 4 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
@@ -24,7 +24,7 @@ WANDB_DISABLED=1 NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 deepspeed --num_gpus 2 --m
     --evaluation_strategy steps \
     --load_best_model_at_end \
     --learning_rate 5e-5 \
-    --num_train_epochs 10.0 \
+    --num_train_epochs 2.0 \
     --max_samples 3000 \
     --val_size 0.1 \
     --plot_loss \
