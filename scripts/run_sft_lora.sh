fa7290df8e1982ee843aaa52d36cdce41ddfbc94
zhangjl19 <zhangjl19@spdb.com.cn>
2024-04-23 08:52:23 +0800

    adjust to new version

--- scripts/run_sft_lora.sh
+++ scripts/run_sft_lora.sh
@@ -10,12 +10,22 @@ WANDB_DISABLED=1 NCCL_P2P_DISABLE=1 NCCL_IB_DISABLE=1 deepspeed --num_gpus 2 --m
     --lora_target query_key_value \
     --output_dir /workspace/models/huggingface/chatglm3-6b_exp_sft_lora_llamafactory \
     --overwrite_cache \
-    --per_device_train_batch_size 4 \
-    --gradient_accumulation_steps 4 \
+    --overwrite_output_dir \
+    --cutoff_len 1024 \
+    --preprocessing_num_workers 16 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 8 \
     --lr_scheduler_type cosine \
     --logging_steps 10 \
-    --save_steps 1000 \
+    --warmup_steps 20 \
+    --save_steps 100 \
+    --eval_steps 100 \
+    --evaluation_strategy steps \
+    --load_best_model_at_end \
     --learning_rate 5e-5 \
     --num_train_epochs 10.0 \
+    --max_samples 3000 \
+    --val_size 0.1 \
     --plot_loss \
     --fp16
